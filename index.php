1381fe3797322c13fd6355e32efc9a1c4d21fe0f
anthony spiteri <anthonyspiteri@hotmail.fr>
2023-02-16 09:31:24 +0100

    ajout autoload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+
+<body>
+	<h1>POO Hotel</h1>
+
+	<?php
+	// récupérer les class des autres fichiers
+
+	spl_autoload_register(function ($class_name) {
+		require_once $class_name . '.php';
+	});
+
+
+	?>
+
+</body>
+
+</html>
